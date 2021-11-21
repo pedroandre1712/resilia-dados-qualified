d0413e58d2624a5277f01fbb49d609b131e29281
Pedro <pedroandrebarrozo@gmail.com>
2021-11-20 22:08:05 -0300

    codigo atualizado

--- qualified_podio.py
+++ qualified_podio.py
@@ -1,7 +1,8 @@
-def podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3):
-    # Implemente aqui a lógica da função
-  a = [tempo_chegada1, tempo_chegada2, tempo_chegada3]
-  a.sort()
+def podio_olimpico(tempo_chegada1, tempo_chegada2,tempo_chegada3, nome_corredor1, nome_corredor2, nome_corredor3):
+  # Implemente aqui a lógica da função
+  a = {tempo_chegada1:nome_corredor1, tempo_chegada2:nome_corredor2, tempo_chegada3:nome_corredor3}
+  b = [tempo_chegada1, tempo_chegada2, tempo_chegada3]
+  b.sort()
   espaco = " minutos\n"
-  variavel_de_retorno = "1 - " + str(a[0]) + espaco + "2 - " + str(a[1]) + espaco + "3 - " + str(a[2]) + espaco
+  variavel_de_retorno = "1 - " + a[b[0]] + " - " + str(b[0]) + espaco +"2 - " + a[b[1]] + " - " + str(b[1]) + espaco +"3 - " + a[b[2]] + " - " + str(b[2]) + espaco
   return variavel_de_retorno
